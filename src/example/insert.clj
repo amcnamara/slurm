ee20783eff2512fde4fd4d33a7b52bc8859a1079
Alex McNamara <alex.mcnamara@gmail.com>
2011-07-29 01:07:27 -0400

    cleaned up example code

--- src/example/insert.clj
+++ src/example/insert.clj
@@ -5,15 +5,17 @@
 (def db-schema (try (slurp "src/example/db-schema") (catch Exception e (println "Failed to open schema file\n" e))))
 
 (defn -main []
-  (let [orm (slurm/init db-schema)]
-    (println (slurm/.field (slurm/.create orm (DBConstruct. :student
-							    {:name "Alex McNamara"
-							     :address (slurm/.create orm (DBConstruct. :address
-												       {:country "Canada"
-													:postal_code "A1B2C3"
-													:province "Quebec"
-													:city "Montreal"
-													:street "St-Jacques"
-													:number 1234
-													:not-a-column "noval"}))}))
-			   :address))))
+  (let [orm        (slurm/init db-schema)
+	my-address (slurm/.create orm (DBConstruct. :address
+						   {:country "Canada"
+						    :postal_code "A1B2C3"
+						    :province "Quebec"
+						    :city "Montreal"
+						    :street "St-Jacques"
+						    :number 1234
+						    :not-a-column "aoeu"}))
+	my-user    (slurm/.create orm (DBConstruct. :student
+						   {:name "Alex McNamara"
+						    :address my-address}))]
+    (println my-user)                           ;; Notice that the address field is lazy loaded
+    (println (slurm/.field my-user :address)))) ;; Fetching the field will load the foreign dbobject
