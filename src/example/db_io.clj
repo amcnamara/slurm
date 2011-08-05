f7113e6f191ee4e7bd9b04c5e0ff522a025db8a0
Alex McNamara <alex.mcnamara@gmail.com>
2011-08-05 00:28:09 -0400

    Cleaned up example code

--- src/example/db_io.clj
+++ src/example/db_io.clj
@@ -7,18 +7,13 @@
 (defn -main []
   ;; Initialize the tables and create some dummy data
   (let [slurmdb    (slurm/init db-schema)
-	course1    (slurm/.create slurmdb (DBConstruct. :course
-						       {:name "Software Design I"}))
-	course2    (slurm/.create slurmdb (DBConstruct. :course
-						       {:name "Intro to Programming"}))
-	cs-degree  (slurm/.create slurmdb (DBConstruct. :degree
-						       {:name "Computer Science, B.Sc."
-						        :courses [course1, course2]}))]
-    (println (slurm/.fetch slurmdb (DBClause. :degree :id := 1)))  ;; Clauses will fetch seqs of valid dbobject records
-                                                                   ;; NOTE: Try changing the :loading value in db-schema from :eager to :lazy
-                                                                   ;;       and see the difference it has on fetches.
-    (println (slurm/.field  cs-degree :courses))                   ;; Fetching a field will load the foreign dbobject(s) if needed (ie. if lazy)
-    (println (slurm/.assoc* cs-degree {:name "Bachelor of CS"}))   ;; Returns a new DBObject and triggers update on DB
-    (slurm/.delete cs-degree)                                      ;; Remove the objects from the DB
+	course1    (slurm/.create slurmdb (DBConstruct. :course {:name "Software Design I"}))
+	course2    (slurm/.create slurmdb (DBConstruct. :course {:name "Intro to Programming"}))
+	cs-degree  (slurm/.create slurmdb (DBConstruct. :degree {:name "Computer Science, B.Sc." :courses [course1, course2]}))]
+    ;; NOTE: Try changing the :loading value in db-schema from :eager to :lazy and see the difference it has on fetches.
+    (println (slurm/.fetch  slurmdb   (DBClause. :degree :id := 1)))  ;; Clauses will fetch seqs of valid dbobject records
+    (println (slurm/.field  cs-degree :courses))                      ;; Fetching a field will load the foreign dbobject(s) if needed (ie. if lazy)
+    (println (slurm/.assoc* cs-degree {:name "Bachelor of CS"}))      ;; Returns a new DBObject and triggers update on DB
+    (slurm/.delete cs-degree)                                         ;; Remove the objects from the DB
     (slurm/.delete course1)
     (slurm/.delete course2)))
