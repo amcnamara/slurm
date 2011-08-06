5ca6776b5baeef035c6166881cb38fdb7450e9ba
Alex McNamara <alex.mcnamara@gmail.com>
2011-08-06 02:56:01 -0400

    Cleaned example, corrected README

--- src/example/db_example.clj
+++ src/example/db_example.clj
@@ -8,13 +8,13 @@
 (defn -main []
   ;; Initialize the tables and create some dummy data
   (let [slurmdb    (slurm/init db-schema)
-	course1    (slurm/.create slurmdb (DBConstruct. :course {:name "Software Design I"}))
-	course2    (slurm/.create slurmdb (DBConstruct. :course {:name "Intro to Programming"}))
-	cs-degree  (slurm/.create slurmdb (DBConstruct. :degree {:name "Computer Science, B.Sc." :courses [course1, course2]}))]
+	course1    (.create slurmdb (DBConstruct. :course {:name "Software Design I"}))
+	course2    (.create slurmdb (DBConstruct. :course {:name "Intro to Programming"}))
+	cs-degree  (.create slurmdb (DBConstruct. :degree {:name "Computer Science, B.Sc." :courses [course1, course2]}))]
     ;; NOTE: Try changing the :loading value in db-schema from :eager to :lazy and see the difference it has on fetches.
-    (println (slurm/.fetch  slurmdb   (DBClause. :degree :id := 1)))  ;; Clauses will fetch seqs of valid dbobject records
-    (println (slurm/.field  cs-degree :courses))                      ;; Fetching a field will load the foreign dbobject(s) if needed (ie. if lazy)
-    (println (slurm/.assoc* cs-degree {:name "Bachelor of CS"}))      ;; Returns a new DBObject and triggers update on DB
-    (slurm/.delete cs-degree)                                         ;; Remove the objects from the DB
-    (slurm/.delete course1)
-    (slurm/.delete course2)))
+    (println (.fetch  slurmdb   (DBClause. :degree :id := 1)))  ;; Clauses will fetch seqs of valid dbobject records
+    (println (.field  cs-degree :courses))                      ;; Fetching a field will load the foreign dbobject(s) if needed (ie. if lazy)
+    (println (.assoc* cs-degree {:name "Bachelor of CS"}))      ;; Returns a new DBObject and triggers update on DB
+    (.delete cs-degree)                                         ;; Remove the objects from the DB
+    (.delete course1)
+    (.delete course2)))
