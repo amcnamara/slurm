2e40da32f6607a134885987c067446804aa39dcd
Alex McNamara <alex.mcnamara@gmail.com>
2011-08-14 19:08:50 -0400

    Updated example code to use all with-orm crud functions

--- src/example/db_example.clj
+++ src/example/db_example.clj
@@ -6,9 +6,19 @@
 
 (defn -main []
   (with-orm db-schema
+    ;; Create some DBOs
     (let [test-course1 (course!  {:name "Software Design I"})
+	  test-course2 (course!  {:name "Algorithms and Data Structures I"})
 	  test-address (address! {:number 2354 :street "Rue St-Jacques" :city "Montreal" :province "QC" :country "Canada"})
-	  test-student (student! {:name "Alex McNamara" :courses [test-course1] :address test-address})]
-      (println "Test student DBO:\n\t"                   test-student)
-      (println "Get DBO for course where id = 1:\n\t"    (course 1))
-      (println "Get DBOs for addresses in Montreal:\n\t" (address :city "Montreal")))))
+	  test-student (student! {:name "Alex McNamara" :courses [test-course1 test-course2] :address test-address})]
+      ;; Fetch the DBO for course where primary-key (:id in this case) is 1
+      (course 1)
+      ;; Fetch a seq of address DBOs from the city of Montreal
+      (address :city "Montreal")
+      ;; Change the test-student record, and get the new DBO
+      (.assoc* test-student {:name "Bender Bending Rodriguez"})
+      ;; Clean up DB
+      (.delete test-course1)
+      (.delete test-course2)
+      (.delete test-address)
+      (.delete test-student))))
